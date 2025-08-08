35f254444144ef47ccfb5d6c59a6fa0062862d51
Alex <AlexStiles123@gmail.com>
2025-08-08 14:52:26 +1000

    .

--- duplicate_values/src/main.rs
+++ duplicate_values/src/main.rs
@@ -7,7 +7,7 @@ impl Solution {
     pub fn contains_duplicate(nums: Vec<i32>) -> bool {
         // Add each of the elements in the vector to the hash set
         let vec_set: HashSet<i32> = HashSet::from_iter(nums.iter().cloned());
-        
+ 
         // Compare the length of the hash set to the array. If they aren't then there are duplicate
         // elements.
         !(vec_set.len() == nums.len())
--- two_sum/src/main.rs
+++ two_sum/src/main.rs
@@ -6,11 +6,14 @@ impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         // Create hash map containing complements required for each element in vector to meet
         // target.
-        let complement_map: HashMap<i32, i32> = HashMap::from_iter(nums.iter().enumerate().map(|(i, num)| (target - num, i as i32)));
+        let complement_map: HashMap<i32, i32> = HashMap::from_iter(
+            nums.iter()
+                .enumerate()
+                .map(|(i, num)| (target - num, i as i32)),
+        );
 
         // Iterate over the nums vector again and see if any num has a valid complement in the hash
         // map.
-        
         let mut result: Vec<i32> = vec![];
         for (i, num) in nums.iter().enumerate() {
             if complement_map.contains_key(num) {
@@ -19,7 +22,7 @@ impl Solution {
 
                 // Ensure candidate isn't the same as the currently evaluated index
                 if candidate != (i as i32) {
-                    result = vec![i as i32, candidate]; 
+                    result = vec![i as i32, candidate];
                     break;
                 }
             }
@@ -30,9 +33,6 @@ impl Solution {
     }
 }
 
-
-
 fn main() {
-    println!("{:?}", Solution::two_sum(vec![3,2,4], 6));
+    println!("{:?}", Solution::two_sum(vec![3, 2, 4], 6));
 }
-
